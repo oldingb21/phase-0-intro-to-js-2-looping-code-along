f223a2c4cc982465f5b394a3d4dbcf2152ac27a3
Brenden Olding <oldingb21@gmail.com>
2023-03-17 00:37:53 -0400

    lab complete

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Code your solutions in this file
+const names = ["Guadalupe", "Ollie", "Aki"];
+
+function writeCards(names, value) {
+    let messages = []
+    for (let n = 0; n < names.length ; n++) {
+        messages.push(`Thank you, ${names[n]}, for the wonderful ${value} gift!`);
+    }
+    return messages
+}
+
+let number 
+
+function countDown(number) {
+    while (number >= 0) {
+        console.log(number);
+        number--
+    }
+}
